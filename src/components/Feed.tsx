9bbc7cc37c3f77642b5eb726aed8902293c173fe
{SUDA-KOUZOU} <{yamanasi1934@gmail.com}>
2021-08-23 10:15:33 +0900

    2021.08.23up

---
+++ src/components/Feed.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "./Feed.module.css";
+import { auth } from "../firebase";
+import TweetInput from './TweetInput';
+
+
+const Feed = () => {
+  return (
+    <div className={styles.feed}>
+      <TweetInput />
+      <button onClick={() => auth.signOut()}>Logout</button>
+    </div>
+  );    
+}
+
+export default Feed
